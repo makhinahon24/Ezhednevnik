ee354c7476f4d6f31e9cc09f76ab1edfd924d70d
marinatursunova <marinatursunova@24C3>
2023-03-23 15:24:23 +0300

    Добавьте файлы проекта.

---
+++ Ezhednevnik/JSON.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+namespace Ezhednevnik
+{
+    public class JSON
+    {
+        public void JsonFileManager(string path, string fileName)
+        {
+            if (!File.Exists(path + fileName))
+            {
+                File.Create(path + fileName);
+            }
+        }
+        public static List<Note> JsonReader(List<Note> notes, string path, string fileName)
+        {
+            notes = new List<Note>();
+
+            if (!File.Exists(path + fileName))
+            {
+                new JSON().JsonFileManager(path, fileName);
+            }
+            else
+            {
+                string jsonString = File.ReadAllText(path + fileName);
+                if (jsonString.Length != 0)
+                {
+                    notes = JsonConvert.DeserializeObject<List<Note>>(jsonString);
+                }
+            }
+            return notes;
+        }
+
+        public static void JsonWriter(string path, string fileName, List<Note> notesJson)
+        {
+            if (!File.Exists(path + fileName))
+            {
+                new JSON().JsonFileManager(path, fileName);
+            }
+            else
+            {
+                string jsonString = JsonConvert.SerializeObject(notesJson);
+                File.WriteAllText(path + fileName, jsonString);
+            }
+        }
+
+    }
+}
---
+++ Ezhednevnik/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezhednevnik
+{
+    public class Note
+    {
+        public string NameNote { get; set; }
+        public string DescriptionNote { get; set; }
+        public DateTime DateNote { get; set; }
+
+    }
+}
